ae601593762fdce7c7db14188f883440bb058377
Kevin Upton <k_upton@live.com>
2017-10-11 18:39:28 +1000

    fixed the migrations

--- src/database/migrations/2016_01_23_234234_setup_merchant_tables.php
+++ src/database/migrations/2016_01_23_234234_setup_merchant_tables.php
@@ -16,12 +16,12 @@ class SetupMerchantTables extends Migration {
             $table->string('cardID', 32)->primary();
             $table->string('cardKey', 64);
             $table->string('ivrCardID', 32);
-            $table->string('cardNumber', 512)->nullable();
+            $table->string('cardNumber', 500)->nullable();
             $table->string('cardName', 128)->nullable();
             $table->char('cardExpiryMonth', 2)->nullable();
             $table->char('cardExpiryYear', 2)->nullable();
-            $table->char('cardNumberFirst', 512)->nullable();
-            $table->char('cardNumberLast', 512)->nullable();
+            $table->string('cardNumberFirst', 500)->nullable();
+            $table->string('cardNumberLast', 500)->nullable();
             $table->dateTime('cardAdded')->nullable();
             $table->unsignedInteger('user_id')->index()->nullable();
             $table->timestamps();
